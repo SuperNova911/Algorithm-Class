f374941f8bf50b00a8a82f691cfd5068c1d9c717
SuperNova911 <suwhan77@naver.com>
2019-04-08 23:47:57 +0900

    정렬 테스트 클래스

---
+++ Sort/src/Main.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int k = scanner.nextInt();
+
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        for (int index = 0; index < n; index++)
+        {
+            list.add(index, scanner.nextInt());
+        }
+        scanner.close();
+
+        RadixSort.Sort(list, k);
+    }
+}
