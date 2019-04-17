4ae6e50479f2a7f2a9ff2a30cbe8f747ad6fcf93
SuperNova911 <suwhan77@naver.com>
2019-04-18 01:20:09 +0900

    정렬 알고리즘들을 자동으로 테스트 해주는 SortingTest 클래스

---
+++ Algorithm/SortingTest.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm
+{
+    public class SortingTest
+    {
+        public static void Main(string[] args)
+        {
+            List<ISortingAlgorithm> Algorithms = new List<ISortingAlgorithm>()
+            {
+                new SelectionSort(),
+            };
+
+            const int TEST_SIZE = 10000;
+            foreach (var algorithm in Algorithms)
+            {
+                TestSortingAlgorithm(algorithm, TEST_SIZE);
+            }
+        }
+
+        private static void TestSortingAlgorithm(ISortingAlgorithm algorithm, int maxArraySize)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+
+            for (int n = 10; n <= maxArraySize; n *= 10)
+            {
+                int[] array = GenerateRandomArray(n);
+
+                stopwatch.Start();
+                algorithm.Sort(array);
+                stopwatch.Stop();
+
+                Console.WriteLine(string.Format("'{0}', IsSorted: '{1}', ElapsedTime: '{2}', N: '{3}'", algorithm.GetType().Name, IsSorted(array), stopwatch.Elapsed, array.Length));
+                stopwatch.Reset();
+            }
+        }
+
+        private static int[] GenerateRandomArray(int arraySize)
+        {
+            int[] newArray = new int[arraySize];
+
+            Random random = new Random();
+
+            for (int index = 0; index < newArray.Length; index++)
+            {
+                newArray[index] = random.Next();
+            }
+
+            return newArray;
+        }
+
+        private static bool IsSorted(int[] array)
+        {
+            if (array.Length <= 1)
+            {
+                return true;
+            }
+
+            for (int index = 1; index < array.Length; index++)
+            {
+                if (array[index - 1] > array[index])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
