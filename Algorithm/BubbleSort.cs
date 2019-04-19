1c8d8885ac68b0a7a727f65fa712714a92b79793
SuperNova911 <suwhan77@naver.com>
2019-04-19 23:58:39 +0900

    버블소트 알고리즘 추가

---
+++ Algorithm/BubbleSort.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm
+{
+    public class BubbleSort : ISortingAlgorithm
+    {
+        public void Sort(int[] array)
+        {
+            if (array.Length < 1)
+            {
+                return;
+            }
+
+            for (int last = array.Length - 1; last >= 2; last--)
+            {
+                bool isSorted = true;
+
+                for (int index = 1; index <= last; index++)
+                {
+                    if (array[index - 1] > array[index])
+                    {
+                        Swap(array, index - 1, index);
+                        isSorted = false;
+                    }
+                }
+
+                if (isSorted == true)
+                {
+                    return;
+                }
+            }
+        }
+
+        private void Swap(int[] array, int x, int y)
+        {
+            int temp = array[x];
+            array[x] = array[y];
+            array[y] = temp;
+        }
+    }
+}
