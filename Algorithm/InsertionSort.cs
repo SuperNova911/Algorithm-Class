8858913e4c1d2f1ae8d6b7e9b41fb0ef4aca122a
SuperNova911 <suwhan77@naver.com>
2019-04-20 23:16:50 +0900

    삽입정렬 구현

---
+++ Algorithm/InsertionSort.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm
+{
+    public class InsertionSort : ISortingAlgorithm
+    {
+        public void Sort(int[] array)
+        {
+            if (array.Length < 1)
+            {
+                return;
+            }
+
+            for (int i = 1; i < array.Length; i++)
+            {
+                int newItem = array[i];
+                int targetIndex = i - 1;
+
+                while (targetIndex >= 1 && newItem < array[targetIndex])
+                {
+                    array[targetIndex + 1] = array[targetIndex];
+                    targetIndex--;
+                }
+
+                array[targetIndex] = newItem;
+            }
+        }
+    }
+}
